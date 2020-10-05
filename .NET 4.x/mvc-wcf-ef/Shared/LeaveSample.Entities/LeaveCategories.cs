961f9179dbfb478dd069ad4b6f6a65f6bf3ec3ae
Serena Yeoh <serena_yeoh@yahoo.com>
2020-10-05 09:44:03 +0800

    Migrate from codeplex and updated to .NET 4.8
    Migrated the source from previous codeplex repository and changed the target framework to 4.8

---
+++ .NET 4.x/mvc-wcf-ef/Shared/LeaveSample.Entities/LeaveCategories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveSample.Entities
+{
+    [Serializable]
+    public enum LeaveCategories : byte
+    {
+        Annual,
+        Medical, 
+        Study
+    }
+}
